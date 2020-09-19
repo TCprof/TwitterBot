230762d9c0c46b511007ca321e4941226187deb9
TomCordall <tomcordall@gmail.com>
2020-09-19 13:12:22 +0100

    added config.py to read in keys and secrets

--- .idea/settings/config.py
+++ .idea/settings/config.py
@@ -5,3 +5,21 @@ import configparser
 filepath = os.path.filepath(__file__)
 name = os.path.dirname(filepath)
 
+config = configparser.ConfigParser()
+config.read(name + '/keys')
+reddit_config = config['Reddit']
+client_id = reddit_config['client_id']
+client_secret = reddit_config['client_secret']
+user_agent = reddit_config["client_secret"]
+
+twitter_config = config['Twitter']
+api_key = twitter_config['api key']
+secret = twitter_config['secret']
+token = twitter_config['token']
+access_token = twitter_config['access_token']
+token_secret = twitter_config['token_secret']
+
+authentication = tweepy.OAuthHandler(api_key,secret)
+authentication.set_access_token(access_token,token_secret)
+api = tweepy.API(authentication)
+
