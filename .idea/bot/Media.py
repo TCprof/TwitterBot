13e4506f8aee1affdc212b3f6c7a5c3509c782f1
TomCordall <tomcordall@gmail.com>
2020-09-24 22:51:41 +0100

    changed update to update_with_media as update is depreciated

--- .idea/bot/Media.py
+++ .idea/bot/Media.py
@@ -1,10 +1,11 @@
 import os
-
+import tweepy
 class Tweet():
     def __init__(self, tweet_media, tweet_text):
         self.text = tweet_text
         self.media = tweet_media
 
     def post(self, api):
-        status = api.update(filename = self.media, status = self.text)
-        return status.id
+        print(self.media)
+        api.update_with_media(filename = self.media, status = self.text)
+        #return status.id
